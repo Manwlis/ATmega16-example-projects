82c1da7e83d2bd34a3bb5f0d419764b8e3989ea9
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-10-09 20:51:22 +0300

    lab 1 5.0 final
    interrupt
    Σταθερά για την τιμή του Compare Reg
    Μετονομασία μεθόδου εξηπηρέτησης interrupt
    
    Αναφορά
    Όλα τα υπόλοιπα

--- 1/code/interrupt/main.asm
+++ 1/code/interrupt/main.asm
@@ -6,16 +6,17 @@
 ; Created: 6/10/2020
 ; Developed with AtmelStudio 7.0.129
 ;
+.equ OCR_value = 155 ; Compare register value
 
 .org $000
 rjmp init ; Start from initialization routine
 .org $026
-rjmp ISR_TOV0
+rjmp ISR_TC0
 
 .org $02A
 ; Initialization routine
 init:
-	; Set stack pointer so program returns from interrupt where it occured
+	; Set stack pointer so program returns from interrupt where it occurred
 	ldi r16, HIGH(RAMEND) ; Upper byte
 	out SPH,r16 ; to stack pointer
 	ldi r16, LOW(RAMEND) ; Lower byte
@@ -28,8 +29,8 @@ init:
 	out TIFR,r16 ; Clear TOV0/ clear pending interrupts
 	ldi r16,1<<OCIE0
 	out TIMSK,r16 ; Enable Timer/Counter0 compare interrupt
-	ldi r16,155 ; Compare register value
-	out OCR0,r16
+	ldi r16,OCR_value
+	out OCR0,r16 ; Set Compare Register
 
 	; Mask used to calculate output
 	clr R17
@@ -43,8 +44,8 @@ init:
 
 loop: rjmp loop ; Infinite loop
 
-; Interrupt service routine
-ISR_TOV0:
+; Interrupt service routine for timer/counter0 compare match mode
+ISR_TC0:
 	in r16,PORTB ; Read Port B
 	eor r16, r17 ; Invert bit 0 of r16 register
 	out PORTB,r16 ; Write Port B
