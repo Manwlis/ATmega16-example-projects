ef5a1e8bc29343f8c0e2614d705ed2e536103647
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-10-14 21:12:23 +0300

    lab2 2.0
    Η αποθύκευση δεδομένων γίνεται στην ρουτίνα write_data αντί στον interrupt handler.
    Τα 7 segment νούμερα αποθυκεύονται στην μνήμη από την ρουτίνα write_7_segments.
    circular counter μετονομάστηκε σε ring counter.
    ring_to_bcd: Μετατρέπει νούμερο ring σε bcd.
    BCD_to_7_segment_Decoder: Επιστρέφει το αντοίστιχο 7 segment από την μνήμη. Κάνει έλεγχο εγκυρότητας.
    ISR_TC0: Εμφανήζει στο PORTA το επόμενο ψηφίο και ενεργοποιεί το κατάλληλο bit του PORTC.
    
    Τεστάρισμα όλα. Φαίνεται να δουλέυουν.

--- 2/code/AssemblerApplication1/main.asm
+++ 2/code/AssemblerApplication1/main.asm
@@ -8,15 +8,16 @@
 ;
 
 //--------------------------------------------------------------------
-// temp regs: r16
-// routine/interrupt param/return regs: r0
-// pointer regs: r26, r27
+// temp regs: r16, r17, r18
+// subroutine/interrupt param/return regs: r0, r1
+// pointer regs: r26, r27, r28, r29
 //--------------------------------------------------------------------
 
 .equ OCR_value = 155 ; Compare register value
-.equ data_pointer = 0x0060 ; potision of SRAM used to store data
+.equ data_pointer = 0x0060 ; position of SRAM used to store data
+.equ segments_pointer = 0x0068 ; position of SRAM used to store 7 segments encodings
 .equ num_of_data = 8 ;
-.equ offset = 0 ; value of first data
+.equ offset = 1 ; value of first data
 
 ; Reset and Interrupt vectors
 .org $000
@@ -37,28 +38,17 @@ init:
 	ldi r16, LOW( RAMEND ) ; Lower byte
 	out SPL,r16 ; to stack pointer
 
-	; Set memory pointer 
-	ldi r27, HIGH( data_pointer ) ; pointer registers X (r26:r27)
-    ldi r26, LOW( data_pointer )
-// Save in SRAM the data that are going to be shown 
-// first_pos_value = offset
-// next_pos_value = prev_pos_value + 1
-// num_of_data times
-//---
-	ldi r16, offset ; starting value
-	set_data_loop:
-		st X+, r16 ; store to SRAM / post increment
-		inc r16	
-		cpi r16, offset + num_of_data
-		brne set_data_loop
-//---
+	; save encoding
+	call write_7_segments
+	; set output
+	call write_data
 
 	; Set Ports A and C as outputs and init
 	ldi r16, 0xFF
 	out DDRA, R16
 	out DDRC, R16
-	out PORTA, R16 ; 1 is off
-	clr r16
+	out PORTA, R16 ; all segments off
+	ldi r16, 0b10000000 ; in order to start from rightmost 7 segment
 	out PORTC, R16
 
 	; Set Timer0 at ~1ms
@@ -79,40 +69,115 @@ loop: rjmp loop
 
 
 //--------------------------------------------------------------------
+// Save in SRAM numbers 0-9 in 7 segment format.
+// arguments: none
+// returns: none
+// changes: r16, r28, r29
+//--------------------------------------------------------------------
+write_7_segments:
+	; Set memory pointer 
+	ldi r29, HIGH( segments_pointer ) ; pointer registers Y (r28:r29)
+    ldi r28, LOW( segments_pointer ) ; index X doesn't support displacement
+
+	; save encodings
+	ldi r16, 0b00000011
+	std Y+0, r16
+
+	ldi r16, 0b10011111
+	std Y+1, r16
+
+	ldi r16, 0b00100101
+	std Y+2, r16
+
+	ldi r16, 0b00001101
+	std Y+3, r16
+
+	ldi r16, 0b10011001
+	std Y+4, r16
+
+	ldi r16, 0b01001001
+	std Y+5, r16
+
+	ldi r16, 0b01000001
+	std Y+6, r16
+
+	ldi r16, 0b00011111
+	std Y+7, r16
+
+	ldi r16, 0b00000001
+	std Y+8, r16
+	
+	ldi r16, 0b00001001
+	std Y+9, r16
+
+	ret
+
+
+//--------------------------------------------------------------------
+// Save in SRAM the data that are going to be shown. LSB to MSB
+// first_pos_value = offset
+// next_pos_value = prev_pos_value + 1
+// num_of_data times
+// arguments: none
+// returns: none
+// changes: r16, r26, r27
+//--------------------------------------------------------------------
+write_data:
+	; Set memory pointer 
+	ldi r27, HIGH( data_pointer ) ; pointer registers X (r26:r27)
+    ldi r26, LOW( data_pointer )
+	ldi r16, offset ; starting value
+	set_data_loop:
+		st X+, r16 ; store to SRAM / post increment
+		inc r16	
+		cpi r16, offset + num_of_data
+		brne set_data_loop
+	ret
+
+
+//--------------------------------------------------------------------
 // Interrupt service routine for timer/counter0 compare match mode.
 // Controls 7 segments outputs.
 // arguments: none
 // returns: none
-// changes: r0, r16
+// changes: r0, r1, r16, r26, r27
 //--------------------------------------------------------------------
 ISR_TC0:
-; TO_DO
-
 	; show nothing
 	ldi r16, 0xFF
 	out PORTA, R16
 	
-	in r0, PORTC ; get currenty enabled 7 seg 
-	rcall circular_counter ; calculate next 7 seg
+	in r0, PORTC ; get currenty enabled 7 seg
+	rcall ring_counter ; calculate next 7 seg
 	out PORTC, r0 ; disable enabled 7 seg and enable next
 
-	;read from mem based on r0 value
+	; read from mem based using ring counter value as offset
+	rcall ring_to_bcd
+	mov r16, r1
+	dec r16 ; 1st number is in pos X+0, 2nd number is in pos X+1, etc.
 
-	; decode, invert and put output
-	rcall BCD_to_7_segment_Decoder
-	com r0 ; 0 = on, 1 = off. Need to invert
-	out PORTA, r0
+	ldi r27, HIGH( data_pointer ) ; pointer registers X (r26:r27)
+    ldi r26, LOW( data_pointer )
+	; add offset to mem pointer registers.
+	clr r17 ; empty reg
+	add r26, r16
+	adc r27, r17
+
+	ld r0, X ; read data in bcd format
+
+	rcall BCD_to_7_segment_Decoder ; decode
+	out PORTA, r1 ; put output
 
 	reti ; clears interrupt flag
 
 
 //--------------------------------------------------------------------
-// Circular shift to the left.
+// Circular shift to the left. ex: 00001000 -> 00010000
 // arguments: input in r0
 // returns: shifted output in r0
 // changes: r0, r16
 //--------------------------------------------------------------------
-circular_counter:
+ring_counter:
 	clr r16 ; empty reg
 	lsl r0 ; Logical Shift Left / bit 0 cleared, bit 7 to carry
 	adc r0, r16 ; Add with carry and empty reg / carry to bit 0
@@ -120,13 +185,46 @@ circular_counter:
 
 
 //--------------------------------------------------------------------
-// Decodes a BCD number (0-9) to 7 segment format
+// Transform a ring number to bcd. ex: 01000000 -> 00000111
+// arguments: ring format number in r0
+// returns: bcd number in r1
+// changes: r0, r1, r16
+//--------------------------------------------------------------------
+ring_to_bcd:
+	clr r1
+	mov r16, r0
+; bcd number = num of shift that are needed to clear ring number.
+rtb_loop:
+	inc r1
+	lsr r16
+	brne rtb_loop
+	ret
+
+
+//--------------------------------------------------------------------
+// Decodes a BCD number (0-9) to 7 segment format. ex: 00000010 -> 00100101
+// 7 segment is in common cathode format.
 // arguments: bcd number in r0
-// returns: 7 segment number in r0
-// changes: r0, r16
+// returns: 7 segment number in r1
+// changes: r1, r16, r17, r18, r26, r27
 //--------------------------------------------------------------------
 BCD_to_7_segment_Decoder:
-; TO_DO
+	; check if data can be translated in 7 segment format
+	mov r16,r0
+	ldi r17, 0b11111111 ; no segment enabled
+	cpi r16, 10
+	brsh bcd_to_7seg_exit ; if data >= 10 (unsigned), skip
 
-	
+	; get 7 segment format
+	ldi r27, HIGH( segments_pointer ) ; pointer registers X (r26:r27)
+    ldi r26, LOW( segments_pointer )
+	; add offset to mem pointer registers.
+	clr r18 ; empty reg
+	add r26, r16
+	adc r27, r18
+
+	ld r17, X ; read 7 segment number
+
+bcd_to_7seg_exit:
+	mov r1, r17
 	ret
