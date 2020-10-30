e162db415b054e445814ae3876fa7a74c2c7a38e
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-10-30 22:31:51 +0200

    lab 4.0
    Μετατροπή main και initialization σε C.
    Μετατροπή interrupt service code σε avr-gcc assembly.
    Header file.
    
    Λίγο τεστάρισμα, φαίνεται να δουλεύουν.

---
+++ 4/code/program/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+USART_driver.S
+
+7_segment_driver.S
+
+program.c
+
---
+++ 4/code/program/program.h
@@ -0,0 +1,33 @@
+﻿/*
+ * program.h
+ *
+ * Macros and enumerations.
+ *
+ * Created: 30/10/2020 12:19:38 μμ
+ * Author : Emmanouil Petrakos
+ */ 
+
+
+#ifndef PROGRAM_H_
+#define PROGRAM_H_
+	
+	// Compare register value
+	#define OCR_value 77
+	// 1 data for each 7 segment
+	#define num_of_data 8
+
+	// Transmitter States
+	#define none 0xFF
+	#define O 0x4F
+	#define K 0x4B
+	#define CR 0x0D
+	#define LF 0x0A
+
+	// ( F_CPU / ( baudrate * 16 ) ) - 1 = ~64,1
+	#define UBBR_value 64
+
+	// LOW, HIGH byte functionality from assembly to C
+	#define low(x)	( (x) & 0xFF )
+	#define high(x)	( ( (x) >> 8 ) & 0xFF )
+
+#endif /* PROGRAM_H_ */
