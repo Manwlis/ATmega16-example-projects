694dc277a5a42acc8ecafd67de4375ebfcbdf6c4
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-11-10 21:23:53 +0200

    lab5 1.0
    ISR( USART_RXC_vect )
    ISR( USART_UDRE_vect )
    ISR( TIMER0_COMP_vect )
    κώδικας ολοκληρώθηκε
    Τεστάρισμα, φαίνεται να δουλεύουν
    
    Αναφορά: Πρόλογος, δομή κώδικα, αρχικοποιήσεις και ISRs

---
+++ 5/code/program/7_segment_driver.c
@@ -0,0 +1,56 @@
+﻿/*
+ * 7_segment_driver.c
+ *
+ * Driver for an 8 digits 7 segment display. Shows data from SRAM.
+ *
+ * Created: 10/11/2020
+ * Author: Emmanouil Petrakos
+ * Developed with AtmelStudio 7.0.129
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+extern unsigned char data[8];
+extern unsigned char segments_encoding[11];
+
+
+//--------------------------------------------------------------------
+// Interrupt service routine for timer/counter0 compare match mode.
+// Controls 7 segments outputs.
+//--------------------------------------------------------------------
+ISR( TIMER0_COMP_vect )
+{
+	// Show nothing
+	PORTA = 0xFF;
+	
+	// Ring counter
+	PORTC = (PORTC >> 7) | (PORTC << 1);
+	
+	// Get data based on ring counter
+	// This block replaces assembly's ring_to_bcd and read from memory 
+	unsigned char bcd_number;
+	if( PORTC == 0b00000001 )
+		bcd_number = data[0];
+	else if( PORTC == 0b00000010 )
+		bcd_number = data[1];
+	else if( PORTC == 0b00000100 )
+		bcd_number = data[2];
+	else if( PORTC == 0b00001000 )
+		bcd_number = data[3];
+	else if( PORTC == 0b00010000 )
+		bcd_number = data[4];
+	else if( PORTC == 0b00100000 )
+		bcd_number = data[5];
+	else if( PORTC == 0b01000000 )
+		bcd_number = data[6];
+	else if( PORTC == 0b10000000 )
+		bcd_number = data[7];
+		
+	// illegal data. PORTA = 0xFF
+	if( bcd_number > 0x0A )
+		return;
+
+	// Out to port
+	PORTA = segments_encoding[bcd_number];
+} // breakpoint here to check 7 segment ports
---
+++ 5/code/program/USART_driver.c
@@ -0,0 +1,119 @@
+﻿/*
+ * USART_driver.c
+ *
+ * Driver for the USART. Controls the data in SRAM for the 7 segment display.
+ * Transmits a response after every incoming message.
+ *
+ * Created: 10/11/2020
+ * Author: Emmanouil Petrakos
+ * Developed with AtmelStudio 7.0.129
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "program.h"
+
+extern unsigned char data[8];
+extern unsigned char transmitter_status;
+extern unsigned char num_transmits_left;
+
+
+//--------------------------------------------------------------------
+// Interrupt service routine for USART receive completed.
+// Communication is errorless and previous/next inputs/states are irrelevant so a state-machine isn't needed.
+// Only the current input is needed to take the appropriate action.
+//--------------------------------------------------------------------
+ISR( USART_RXC_vect )
+{
+	// Receive frame
+	unsigned char received_frame = UDR;
+	// for debug
+	received_frame = UDR;
+	asm( "mov %0 , r20" : "=r" (received_frame) );
+	
+	if( received_frame == 0x43 ) // C
+		for( unsigned char i = 0 ; i < num_of_data ; i++ )
+			data[i] = 0x0A; // Clear data
+			
+	else if( received_frame == 0x4E ) // N
+		for( unsigned char i = 0 ; i < num_of_data ; i++ )
+			data[i] = 0x0A; // Clear data
+	
+	else if( received_frame == 0x41 ) // A
+		return; // Do nothing
+		
+	else if( received_frame == 0x54 ) // T
+		return; // Do nothing
+		
+	else if( received_frame == 0x0D ) // <CR>
+		return; // Do nothing
+		
+	else if( received_frame == 0x0A ) // <LF>
+	{
+		 // Increase pending responses counter
+		num_transmits_left++;
+		// Enable transmitter interrupts to start the response. If it is already enabled, nothing changes.
+		UCSRB |= ( 1 << UDRIE ); // breakpoint here to check memory after message
+	}
+	else // Frame is a number
+	{
+		// Moves all data one position forward. Top element gets discarded.
+		for( unsigned char i = num_of_data - 1 ; i != 0xFF ; i-- ) // last element is 0xFF
+			data[ i + 1 ] = data[i];
+		// Save new data. Top half byte cleared, ascii -> bcd
+		data[0] = received_frame & 0x0F;
+	}
+}
+
+
+//--------------------------------------------------------------------
+// Interrupt service routine for USART transmitter completed.
+// FSM is one way (see report), input is irrelevant. Only current state is needed to compute next state.
+//--------------------------------------------------------------------
+ISR( USART_UDRE_vect )
+{
+	if( transmitter_status == none )
+	{
+		// Decrease pending responses
+		num_transmits_left--;
+		// Change State
+		transmitter_status = 'O';
+		// Send character
+		UDR = 'O';
+		// Transmitter's UDR is write-only and can't be read by the simulator.
+		// TCNT2 is used for logging.
+		TCNT2 = 'O';
+	}
+	else if( transmitter_status == 'O' )
+	{
+		// Change State
+		transmitter_status = 'K';
+		// Send character
+		UDR = 'K';
+		TCNT2 = 'K';
+	}
+	else if( transmitter_status == 'K' )
+	{
+		// Change State
+		transmitter_status = 0x0D; // CR
+		// Send character
+		UDR = 0x0D;
+		TCNT2 = 0x0D;
+	}
+	else if( transmitter_status == 0x0D )
+	{
+		// Change State
+		transmitter_status = 0x0A; // LF
+		// Send character
+		UDR = 0x0A;
+		TCNT2 = 0x0A;
+	}
+	else if( transmitter_status == 0x0A ) // LF
+	{
+		// Change State
+		transmitter_status = none;
+		// If no more transmits required, disable transmitter interrupts.
+		if( num_transmits_left == 0 )
+			UCSRB &= ~( 1 << UDRIE );
+	}
+}
