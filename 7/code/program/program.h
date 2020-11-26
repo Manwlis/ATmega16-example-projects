04bc321724b8481ba418317db53db73b7196968e
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-11-26 17:06:43 +0200

    lab7 0.0
    Initial commit, κώδικας από εργαστήριο 5 με μικροδιωρθώσεις σε σχόλια και inits

---
+++ 7/code/program/program.h
@@ -0,0 +1,23 @@
+﻿/*
+ * program.h
+ *
+ * Macros and enumerations.
+ *
+ * Created: 30/10/2020
+ * Author : Emmanouil Petrakos
+ * Developed with AtmelStudio 7.0.129
+ */ 
+
+
+#ifndef PROGRAM_H_
+#define PROGRAM_H_
+	
+	#define	F_CPU	10000000
+	#define	BAUD		9600
+	
+	// Compare register value
+	#define OCR_value 77
+	// 1 data for each 7 segment
+	#define num_of_data 8
+	
+#endif /* PROGRAM_H_ */
