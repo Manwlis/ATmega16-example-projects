56db458db322130de10ee2dc6fec39a21073b7c7
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-12-08 21:26:34 +0200

    lab5 STK500
    USART έφυγε ο κώδικας για τον simulator. Το baudrate υπολογίζεται από τον compiler.
    7 Segments μείωση μέγιστης τιμής μετρητή γιατί έπεσε το ρολόι
    
    Δουλεύουν όλα :smiley:

---
+++ 5 STK500/code/program/program.h
@@ -0,0 +1,33 @@
+﻿/*
+ * program.h
+ *
+ * Macros and enumerations.
+ *
+ * Created: 30/10/2020
+ * Author : Emmanouil Petrakos
+ * Developed with AtmelStudio 7.0.129
+ */ 
+
+
+#ifndef PROGRAM_H_
+#define PROGRAM_H_
+	
+	#define	F_CPU	1000000
+	#define	BAUD	9600
+	
+	// Compare register value
+	#define OCR_value 7
+	// 1 data for each 7 segment
+	#define num_of_data 8
+
+	// Transmitter States
+	#define none 0xFF
+
+	// ( F_CPU / ( baudrate * 16 ) ) - 1 = ~64,1
+	#define UBBR_value 64
+
+	// LOW, HIGH byte functionality from assembly to C
+	#define low(x)	( (x) & 0xFF )
+	#define high(x)	( ( (x) >> 8 ) & 0xFF )
+
+#endif /* PROGRAM_H_ */
